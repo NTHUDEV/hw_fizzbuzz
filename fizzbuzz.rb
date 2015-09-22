19462ecf4d029562082c583d3b7e4b54bf7a3b3c
Cesar Ordonez <Cesar@Cesars-MacBook-Pro.local>
2015-09-22 18:46:17 +0800

    2015-09-22: Created logic for FizzBuzz game

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 ## write your fizzbuzz method in this file
 # see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+def fizzbuzz(num)
+  (1..num).map do |a|
+    if a % 3 == 0 && a % 5 == 0
+      'FizzBuzz'
+    elsif a % 3 == 0
+      'Fizz'
+    elsif a % 5 == 0
+      'Buzz'
+    else
+      a
+    end
+  end
+end
